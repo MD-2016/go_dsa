12ce44fb1e9940610d4cdf1b54f0e9ac2565eb34
MD <25043596+MD-2016@users.noreply.github.com>
2023-05-08 21:39:46 -0400

    Added another version of array stack. Going to make another using Generics

--- DS/Stack/arraystack/arraystack2.go
+++ DS/Stack/arraystack/arraystack2.go
@@ -2,6 +2,7 @@ package arraystack2
 
 import (
 	"errors"
+	"fmt"
 )
 
 type Stack struct {
@@ -34,4 +35,26 @@ func (stack *Stack) pop() (interface{}, error) {
 	popped := stack.elements[len(stack.elements)-1]
 	stack.elements = stack.elements[:len(stack.elements)-1]
 	return popped, nil
+}
+
+func (stack *Stack) peek() (interface{}, error) {
+	if stack.isEmpty() {
+		return nil, errors.New("Stack is empty no way to peek")
+	}
+	elm := stack.elements[len(stack.elements)-1]
+	return elm, nil
+}
+
+// assumes the stack isn't empty
+func (stack *Stack) reverse() []interface{} {
+	stackLen := len(stack.elements)
+	reversedStack := make([]interface{}, stackLen)
+	for i, elm := range stack.elements {
+		reversedStack[stackLen-1-i] = elm
+	}
+	return reversedStack
+}
+
+func (stack *Stack) toString() string {
+	return fmt.Sprintf("%v", stack.elements...)
 }
