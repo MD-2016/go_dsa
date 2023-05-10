e0e4680c7c22fc82ebc075040d75b179b295ad6f
MD <25043596+MD-2016@users.noreply.github.com>
2023-05-09 20:30:49 -0400

    array stack with generics is completed but needs tested

--- DS/Stack/arraystack/arraystack3.go
+++ DS/Stack/arraystack/arraystack3.go
@@ -1,13 +1,61 @@
 package arraystack3
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Stack[T any] struct {
 	elements []T
 }
 
-func New[T any]() *Stack[T] {
+func new[T any]() *Stack[T] {
 	return &Stack[T]{nil}
 }
 
 func (stack *Stack[T]) size() int {
 	return len(stack.elements)
+}
+
+func (stack *Stack[T]) isEmpty() bool {
+	return len(stack.elements) == 0
+}
+
+func (stack *Stack[T]) peek() (T, error) {
+	var empty T
+	if stack.isEmpty() {
+		return empty, errors.New("Stack is empty. Cannot peek\n")
+	}
+
+	elm := stack.elements[len(stack.elements)-1]
+	return elm, nil
+}
+
+func (stack *Stack[T]) push(element T) {
+	stack.elements = append(stack.elements, element)
+}
+
+func (stack *Stack[T]) pop() (T, error) {
+	var empty T
+	if stack.isEmpty() {
+		return empty, errors.New("Stack is empty. Cannot pop")
+	}
+
+	popped := stack.elements[len(stack.elements)-1]
+	stack.elements = stack.elements[:len(stack.elements)-1]
+	return popped, nil
+}
+
+func (stack *Stack[T]) toString() string {
+	return fmt.Sprintf("%v", stack.elements)
+}
+
+func (stack *Stack[T]) reverse() []T {
+	stackL := len(stack.elements)
+	reverseStack := make([]T, stackL)
+	for i, elm := range stack.elements {
+		reverseStack[stackL-1-i] = elm
+	}
+
+	return reverseStack
 }
