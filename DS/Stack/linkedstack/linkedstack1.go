4e0eb909dcdd86179e9c4013e86741480448cbe3
MD <25043596+MD-2016@users.noreply.github.com>
2023-05-11 21:49:34 -0400

    Started linked list stackversion 1

---
+++ DS/Stack/linkedstack/linkedstack1.go
@@ -0,0 +1,49 @@
+package linkedstack1
+
+import "errors"
+
+type node struct {
+	items interface{}
+	next  *node
+}
+
+type Stack struct {
+	head   *node
+	length int
+}
+
+func New() *Stack {
+	return &Stack{}
+}
+
+func (stack *Stack) size() int {
+	return stack.length
+}
+
+func (stack *Stack) isEmpty() bool {
+	return stack.length == 0
+}
+
+func (stack *Stack) peek() (interface{}, error) {
+	if stack.length == 0 {
+		return nil, errors.New("Stack is empty. Can't peek!")
+	}
+
+	return stack.head.items, nil
+}
+
+func (stack *Stack) pop() (interface{}, error) {
+	if stack.length == 0 {
+		return nil, errors.New("Stack is empty. Can't pop!")
+	}
+	item := stack.head.items
+	nextItem := stack.head.next
+	stack.head.next = nil
+	stack.head = nextItem
+	stack.length--
+	return item, nil
+}
+
+func (stack *Stack) push(item interface{}) {
+
+}
